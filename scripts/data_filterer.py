b450c483f921b90d284bf3eae721a14f790e4207
day Lee <dayeong.lee@uwaterloo.ca>
2018-07-12 17:29:35 -0400

    Better data filterer to properly filter subtitles file

--- scripts/data_filterer.py
+++ scripts/data_filterer.py
@@ -4,40 +4,65 @@ import re
 import os
 import datetime
 
+round = re.compile('\(.+?\)')
+html = re.compile('\<.+?\>')
+curly = re.compile('\{.+?\}')
+square = re.compile('\[.+?\]')
+
+def clean_string(s):
+    s = re.sub(r'[^a-zA-Z,.?! \'-]', u'', s)
+    s = re.sub(r'- ', u'', s)
+    s = re.sub(r' -', u'', s)
+    if len(s) > 0 and (s[0] == '-' or s[0] == '.'):
+        s = s[1:]
+    return s
+
 # Returns a list of filtered lines from the specified subtitle
 def get_filtered_data(filename):
     print("Filtering ", filename, "\r\n")
     filtered = []
     with open(filename, 'r') as f:
+        space = True
         lines = f.readlines()
         dialogue = []
+        if lines[0][0] == '{':
+            space = False
+
         for l in lines:
             if l.strip().isdigit():
                 continue
-            if "-->" in l or '[' in l or '(' in l:
+            if space and ("-->" in l or len(re.findall(r"(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)", l)) > 0):
                 continue
-            if l.isspace():
+            if space and l.isspace():
                 # process all
                 full_s = ""
                 for s in dialogue:
-                    s = re.sub(r'[^a-zA-Z,.?! \'-]', u'', s)
-                    s = re.sub(r'- ', u'', s)
-                    s = re.sub(r' -', u'', s)
-                    if len(s) > 0 and s[0] == '-':
-                        s = s[1:]
+                    s = clean_string(s)
                     full_s = full_s + " " + s.strip()
                 full_s = full_s.strip()
-                if len(full_s) > 3:
+                if len(full_s.split(' ')) > 1:
                     # print("processing: ", full_s)
                     filtered.append(full_s)
                 dialogue = []
             else:
-                l = l.replace('<i>', '')
-                l = l.replace('</i>', '')
+                l = round.sub('', l)
+                l = curly.sub('', l)
+                l = square.sub('', l)
+                l = html.sub('', l)
+                l = l.replace('|', ' ')
+                l = l.replace('. . .', '.')
                 l = l.replace('\n', '')
                 l = l.replace('\r', '')
                 l = l.strip()
+                # print(l)
                 dialogue.append(l)
+
+        if not space:
+            for s in dialogue:
+                s = clean_string(s)
+                if len(s.split(' ')) > 1:
+                    # print("processing: ", s)
+                    filtered.append(s)
 
     return filtered
 
