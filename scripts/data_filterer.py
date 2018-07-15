ff5fe3333da53e4684ebc2ad5abf08d24d8038e2
day Lee <dayeong.lee@uwaterloo.ca>
2018-07-15 04:33:59 -0400

    Different methods for handling different datatypes (subs or counselling)

--- scripts/data_filterer.py
+++ scripts/data_filterer.py
@@ -9,6 +9,8 @@ html = re.compile('\<.+?\>')
 curly = re.compile('\{.+?\}')
 square = re.compile('\[.+?\]')
 
+speaker = re.compile('^.+?: ')
+
 def clean_string(s):
     s = round.sub('', s)
     s = curly.sub('', s)
@@ -27,34 +29,38 @@ def clean_string(s):
     return s.strip()
 
 # Returns a list of filtered lines from the specified subtitle
-def get_filtered_data(filename):
+def get_filtered_data(filename, data_type):
     print("Filtering ", filename, "\r\n")
-    filtered = []
+    final = []
     with open(filename, 'r') as f:
 
         '''
         Two ways that subtitles are formatted:
-        1. inline
+        1. subs_inline
             {330}{363} some sentence
 
-        2. not inline
+        2. not subs_inline
             1
             00:00:02,292 --> 00:00:03,645  X1:197 X2:518 Y1:484 Y2:524
             some sentence
         '''
-        inline = False
+        subs_inline = None
 
         lines = f.readlines()
         dialogue = []
-        if lines[0][0] == '{':
-            inline = True
+
+        if data_type == "subs":
+            if lines[0][0] == '{':
+                subs_inline = True
+            else:
+                subs_inline = False
 
         for l in lines:
             if l.strip().isdigit():
                 continue
-            if not inline and ("-->" in l or len(re.findall(r"(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)", l)) > 0):
+            if data_type == "subs" and not subs_inline and ("-->" in l or len(re.findall(r"(?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)", l)) > 0): # garbage lines
                 continue
-            if not inline and l.isspace():
+            if data_type == "subs" and not subs_inline and l.isspace(): # form one sentence using multiple lines of sentences
                 full_s = ""
                 for s in dialogue:
                     # Merge fragmented sub sentences into one sentence
@@ -63,33 +69,42 @@ def get_filtered_data(filename):
                 full_s = full_s.strip()
                 if len(full_s) > 0:
                     # print("processing: ", full_s)
-                    filtered.append(full_s)
+                    final.append(full_s)
                 dialogue = []
             else:
                 l = l.strip()
-                if len(l) > 0 and l[0] == '-':
-                    # If begins with -, separate person speaking
+                if data_type == "subs":
+                    if subs_inline or (not subs_inline and len(l) > 0 and l[0] == '-'):
+                        # If begins with -, separate person speaking
+                        l = clean_string(l)
+                        if len(l) > 0:
+                            final.append(l)
+                    elif not subs_inline:
+                        # It could be one sentence written over multiple lines
+                        dialogue.append(l)
+                elif data_type == "transcript":
+                    l = speaker.sub('', l)
                     l = clean_string(l)
-                    filtered.append(l)
-                else:
-                    # It could be one sentence written over multiple lines
-                    dialogue.append(l)
+                    if len(l) > 0:
+                        final.append(l)
 
-        if inline:
+        if data_type == "subs":
             for s in dialogue:
                 s = clean_string(s)
-                if len(s.split(' ')) > 1:
+                if len(s) > 0:
                     # print("processing: ", s)
-                    filtered.append(s)
+                    final.append(s)
 
-    return filtered
+    return final
 
 def main():
-    if len(sys.argv) != 2:
-        print("Enter in the directory name")
+    if len(sys.argv) != 3:
+        print("python data_filterer.py <dir_name> <data_type: subs | transcript >")
         return
 
     dir_name = sys.argv[1]
+
+    data_type = sys.argv[2]
 
     filenames = []
     for file in os.listdir(dir_name):
@@ -99,7 +114,7 @@ def main():
 
     training_data = []
     for filename in filenames:
-        training_data.extend(get_filtered_data(filename))
+        training_data.extend(get_filtered_data(filename, data_type))
         training_data.append("===")
 
     new_filename = dir_name.replace('/', '') + ".txt"
